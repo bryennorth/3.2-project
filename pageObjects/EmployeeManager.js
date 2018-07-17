f0e26ec00fee6a7418feec7fa47dd017512ea6f0
Bryen North <bryen.north@gmail.com>
2018-07-17 14:31:16 -0600

    initial commit

---
+++ pageObjects/EmployeeManager.js
@@ -0,0 +1,59 @@
+module.exports = {
+    url: 'https://devmountain-qa.github.io/employee-manager/1.2_Version/index.html',
+    elements: {
+        //MISC
+        pageHeader: 'span[class="titleText"]',
+        footer: '.footer',
+        //Buttons
+        saveButton: 'button[id="saveBtn"]',
+        cancelButton: 'button[name="cancel"]',
+        disabledSaveButton: '',
+        disabledCancelButton: '',
+        //Infocard
+        Infocard: 'div[class=infoCard]',
+        noEmployee: 'p[id="noEmployee"]',
+        employeeID: 'span[name=employeeID]',
+        employeeName: 'p[id=employeeTitle]',
+        NAME: {
+            selector: '(//span[@class="placeholderText"])[1]',
+            locateStrategy: 'xpath',
+        },
+        nameInputField: 'input[name=nameEntry]',
+        PHONENUMBER: {
+            selector: '(//span[@class="placeholderText"])[2]',
+            locateStrategy: 'xpath',
+        },
+        phoneNumberInputField: 'input[name=phoneEntry]',
+        TITLE: {
+            selector: '(//span[@class="placeholderText"])[3]',
+            locateStrategy: 'xpath',
+        },
+        titleInputField: 'input[name=titleEntry]',
+        nameError: {
+            selector: '//span[contains(.,"The name field must be between 1 and 30 characters long.")]',
+            locateStrategy: 'xpath',
+        },
+        phoneError: {
+            selector: '//span[contains(.,"The phone number must be 10 digits long.")]',
+            locateStrategy: 'xpath',
+        },
+        titleError: {
+            selector: '//span[contains(.,"The title field must be between 1 and 30 characters long.")]',
+            locateStrategy: 'xpath',
+        },
+        //SelectableSidebarEmployees
+        emp1: 'li[name=employee1]',
+        emp2: 'li[name=employee2]',
+        emp3: 'li[name=employee3]',
+        emp4: 'li[name=employee4]',
+        emp5: 'li[name=employee5]',
+        emp6: 'li[name=employee6]',
+        emp7: 'li[name=employee7]',
+        emp8: 'li[name=employee8]',
+        emp9: 'li[name=employee9]',
+        emp10: 'li[name=employee10]',
+        emp11: 'li[name=employee11]',
+        add: 'li[name="addEmployee"]'
+
+    }
+}
---
+++ tests/test.js
@@ -0,0 +1,136 @@
+let inputData = {
+    nameInput: 'Donquixote Doflamingo',
+    phoneInput: '5555555555',
+    titleInput: 'Warlord',
+    badNameInput: 'Our Market Research Shows That Players Like Really Long Card Names So We Made this Card to Have the Absolute Longest Card Name Ever Elemental',
+    badPhoneInput: 'Art rampage 2 (Whenever this creature becomes blocked by a creature, it gets +2/+2 for each creature in the blockers art beyond the first.)',
+    badTitleInput: 'Just call it OMRSTPLRLCNSWMTCTHTALCNEE for short.'
+}
+
+let EmployeeManager = {}
+module.exports = {
+    before: browser => {
+        EmployeeManager = browser.page.EmployeeManager()
+        EmployeeManager.navigate()
+    },
+    after: browser => {
+        browser.end()
+    },
+    // https://dmutah.atlassian.net/browse/DEM-2, https://dmutah.atlassian.net/browse/DEM-9
+    'Initial Page Load and Adding an Employee': browser => {
+        EmployeeManager
+        .waitForElementPresent('@emp1', 5000)
+        .verify.containsText('@pageHeader', 'Employee Manager')
+        EmployeeManager
+        .verify.containsText('@footer', 'Version 1.2')
+        EmployeeManager
+        .waitForElementPresent('@noEmployee', 200)
+        .verify.containsText('@emp1', 'Bernice Ortiz')
+        .verify.containsText('@emp2', 'Marnie Barnett')
+        .verify.containsText('@emp3', 'Phillip Weaver')
+        .verify.containsText('@emp4', 'Teresa Osborne')
+        .verify.containsText('@emp5', 'Dollie Berry')
+        .verify.containsText('@emp6', 'Harriet Williamson')
+        .verify.containsText('@emp7', 'Ruby Estrada')
+        .verify.containsText('@emp8', 'Lou White')
+        .verify.containsText('@emp9', 'Eve Sparks')
+        .verify.containsText('@emp10', 'Lois Brewer')
+        .click('@emp1')
+        .click('@emp2')
+        .click('@emp3')
+        .click('@emp4')
+        .click('@emp5')
+        .click('@emp6')
+        .click('@emp7')
+        .click('@emp8')
+        .click('@emp9')
+        .click('@emp10')
+        .click('@add')
+        .click('@emp11')
+        .assert.valueContains('@nameInputField', 'New Employee')
+        EmployeeManager
+        .assert.valueContains('@phoneNumberInputField', '1234567890')
+        EmployeeManager
+        .assert.valueContains('@titleInputField', 'New Employee')
+        EmployeeManager
+    },
+    //https://dmutah.atlassian.net/browse/DEM-7, https://dmutah.atlassian.net/browse/DEM-8, https://dmutah.atlassian.net/browse/DEM-10
+    'Generating Errors, Clearing Errors, Correcting Errors': browser => {
+        EmployeeManager
+        .setValue('@nameInputField', inputData.badNameInput)
+        .setValue('@phoneNumberInputField', inputData.badPhoneInput)
+        .setValue('@titleInputField', inputData.badTitleInput)
+        .click('@saveButton')
+        .waitForElementPresent('@nameError', 500)
+        .waitForElementPresent('@phoneError', 500)
+        .waitForElementPresent('@titleError', 500)
+        .click('@cancelButton')
+        .waitForElementNotPresent('@nameError', 500)
+        .waitForElementNotPresent('@phoneError', 500)
+        .waitForElementNotPresent('@titleError', 500)
+        .clearValue('@nameInputField')
+        .clearValue('@phoneNumberInputField')
+        .clearValue('@titleInputField')
+        .setValue('@nameInputField', inputData.badNameInput)
+        .setValue('@phoneNumberInputField', inputData.badPhoneInput)
+        .setValue('@titleInputField', inputData.badTitleInput)
+        .click('@saveButton')
+        .waitForElementPresent('@nameError', 500)
+        .waitForElementPresent('@phoneError', 500)
+        .waitForElementPresent('@titleError', 500)
+        .clearValue('@nameInputField')
+        .setValue('@nameInputField', inputData.nameInput)
+        .clearValue('@phoneNumberInputField')
+        .clearValue('@titleInputField')
+        .setValue('@phoneNumberInputField', inputData.phoneInput)
+        .setValue('@titleInputField', inputData.titleInput)
+        .click('@saveButton')
+        .waitForElementNotPresent('@nameError', 500)
+        .waitForElementNotPresent('@phoneError', 500)
+        .waitForElementNotPresent('@titleError', 500)
+    },
+    //https://dmutah.atlassian.net/browse/DEM-6, https://dmutah.atlassian.net/browse/DEM-4,https://dmutah.atlassian.net/browse/DEM-5
+    'Navigating Away, Cancelling and Saving Changes': browser => {
+            EmployeeManager
+            .click('@emp5')
+            .verify.valueContains('@nameInputField', 'Dollie Berry')
+            .verify.valueContains('@phoneNumberInputField', '4873459812')
+            .verify.valueContains('@titleInputField', 'Front-End Developer')
+            .setValue('@nameInputField', inputData.badNameInput)
+            .setValue('@phoneNumberInputField', inputData.badPhoneInput)
+            .setValue('@titleInputField', inputData.badTitleInput)
+            .click('@emp6')
+            .verify.containsText('@employeeID', '6')
+            .click('@emp5')
+            .verify.valueContains('@nameInputField', 'Dollie Berry')
+            .verify.valueContains('@phoneNumberInputField', '4873459812')
+            .verify.valueContains('@titleInputField', 'Front-End Developer')
+            .setValue('@nameInputField', inputData.badNameInput)
+            .setValue('@phoneNumberInputField', inputData.badPhoneInput)
+            .setValue('@titleInputField', inputData.badTitleInput)
+            .click('@cancelButton')
+            .verify.valueContains('@nameInputField', 'Dollie Berry')
+            .verify.valueContains('@phoneNumberInputField', '4873459812')
+            .verify.valueContains('@titleInputField', 'Front-End Developer')
+            .clearValue('@nameInputField')
+            .setValue('@nameInputField', inputData.nameInput)
+            .clearValue('@phoneNumberInputField')
+            .clearValue('@titleInputField')
+            .setValue('@phoneNumberInputField', inputData.phoneInput)
+            .setValue('@titleInputField', inputData.titleInput)
+            .click('@saveButton')
+            .waitForElementNotPresent('@nameError', 500)
+            .waitForElementNotPresent('@phoneError', 500)
+            .waitForElementNotPresent('@titleError', 500)
+            .verify.containsText('@emp5', inputData.nameInput)
+            .click('@emp7')
+            .verify.valueContains('@nameInputField', 'Ruby Estrada')
+            .verify.valueContains('@phoneNumberInputField', '5740923478')
+            .verify.valueContains('@titleInputField', 'Back-End Developer')
+            .click('@emp5')
+            .verify.valueContains('@nameInputField', inputData.nameInput)
+            .verify.valueContains('@phoneNumberInputField', inputData.phoneInput)
+            .verify.valueContains('@titleInputField', inputData.titleInput)
+
+    },
+}
